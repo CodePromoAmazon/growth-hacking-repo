e2d04d23648e2ab876690d491f5e34b569b91c87
Stephen <85037932+SALESHACKING@users.noreply.github.com>
2022-02-12 22:03:11 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,10 @@ export default function Home() {
           Des applications SaaS
         </p>
     
+        <a class="twitter-timeline" href="https://twitter.com/StephenMesnild1?ref_src=twsrc%5Etfw"></a> 
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8">
+        </script>
+    
       </main>
 
       <Footer />
